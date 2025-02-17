49684765103550cef39781c67b4dc623ef4b0e8c
Sarah Reichelt <sarah@troz.net>
2025-02-17 15:50:02 +1000

    add non-functional menus

--- MacMenuComms/MacMenuCommsApp.swift
+++ MacMenuComms/MacMenuCommsApp.swift
@@ -8,5 +8,31 @@ struct MacMenuCommsApp: App {
     WindowGroup {
       ContentView()
     }
+    .commands {
+      CommandMenu("Symbol") {
+        Menu("Symbol") {
+          ForEach(["globe", "house", "display", "car.rear"], id: \.self) { name in
+            Button(name) {
+              // action to change icon
+            }
+          }
+        }
+
+        Menu("Color") {
+          ForEach([Color.blue, Color.red, Color.green, Color.yellow], id: \.self) { color in
+            Button(color.description) {
+              // action to shange color
+            }
+          }
+        }
+
+        Divider()
+
+        Button("Random") {
+          // action to choose random icon & color
+        }
+        .keyboardShortcut("r")
+      }
+    }
   }
 }
