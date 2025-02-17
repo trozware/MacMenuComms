8aea5fabb521a395e0d77b0f93280957184b0b90
Sarah Reichelt <sarah@troz.net>
2025-02-17 15:42:43 +1000

    Initial Commit

---
+++ MacMenuComms/ContentView.swift
@@ -0,0 +1,19 @@
+// Created on 17/2/2025.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacMenuComms/MacMenuCommsApp.swift
@@ -0,0 +1,12 @@
+// Created on 17/2/2025.
+
+import SwiftUI
+
+@main
+struct MacMenuCommsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
